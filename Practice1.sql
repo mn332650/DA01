7653a077027dc45c2fc1f41431ebd318cbafae81
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-01 09:44:15 -0700

    Update Practice1.sql

--- Practice1.sql
+++ Practice1.sql
@@ -12,11 +12,11 @@ from station;
 --ex4: starting with vowels
 select distinct city 
 from station 
-where city like 'a%'
-or city like 'e%'
-or city like 'i%'
-or city like 'o%'
-or city like 'u%';
+where city like 'A%'
+or city like 'E%'
+or city like 'I%'
+or city like 'O%'
+or city like 'U%';
 --ex5: ending with vowels
 select distinct city 
 from station 
@@ -25,7 +25,7 @@ or city like '%e'
 or city like '%i'
 or city like '%o'
 or city like '%u';
---ex6: not starting with vowels
+--ex6: not starting with vowels, pay attention to capital letters
 select distinct city
 from station 
 where city not like 'U%'
@@ -33,3 +33,4 @@ and city not like 'E%'
 and city not like 'O%'
 and city not like 'A%'
 and city not like 'I%';
+--ex7: 
