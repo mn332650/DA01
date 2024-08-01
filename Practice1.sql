79cfdce4e7a5f44b40617b51918c54bb901d727d
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-01 10:14:40 -0700

    Update Practice1.sql

--- Practice1.sql
+++ Practice1.sql
@@ -37,3 +37,43 @@ and city not like 'I%';
 select name 
 from employee
 order by name asc;
+--ex8: 
+select name 
+from employee
+where salary>2000 and months<10
+order by employee_id asc;
+--ex9: 
+select product_id
+from products
+where low_fats = 'Y' and recyclable ='Y'
+--ex10: customer who don't get refered by customer with id=2
+select name
+from customer
+where referee_id <> 2 or referee_id is null;
+--ex11:
+select name, population, area
+from world
+where population >= 25000000 or area >= 3000000;
+--ex12:
+select distinct author_id as id
+from views
+where author_id = viewer_id 
+order by id asc;
+--ex13: 
+SELECT part, assembly_step
+from parts_assembly
+where finish_date is NULL;
+--ex14: 
+select *
+from lyft_drivers
+where yearly_salary <= 30000 or yearly_salary >70000;
+--ex15: 
+select advertising_channel
+from uber_advertising
+where money_spent >100000 and year = '2019';
+
+
+
+
+
+
