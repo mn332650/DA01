9a7b8ac83ab13e7ddbd959142b7c50b7740809b2
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-05 18:34:45 -0700

    Update Practice3.sql

--- Practice3.sql
+++ Practice3.sql
@@ -29,6 +29,16 @@ FROM pharmacy_sales
 group by manufacturer
 order by sum(total_sales) desc, manufacturer;
 
+/*ex4: retrieve the average star rating for each product, grouped by month.
+The output should display the month as a numerical value, product ID, and average star rating rounded to two decimal places.
+Sort the output first by month and then by product ID */
+
+SELECT extract(month from submit_date) as mth,
+product_id as product,
+round(avg(stars),2) as avg_stars
+FROM reviews
+group by extract(month from submit_date),product_id
+order by mth
 
 
 
