a1ce1f0ecc9041341d1bcb80e88e8b4d053a6812
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-05 19:48:47 -0700

    Update Practice3.sql

--- Practice3.sql
+++ Practice3.sql
@@ -38,11 +38,59 @@ product_id as product,
 round(avg(stars),2) as avg_stars
 FROM reviews
 group by extract(month from submit_date),product_id
-order by mth
+order by mth;
 
+/*Ex5: identify the top 2 Power Users who sent the highest number of messages on Microsoft Teams in August 2022.
+Display the IDs of these 2 users along with the total number of messages they sent. 
+Output the results in descending order based on the count of the messages */
 
+SELECT sender_id,
+count(distinct message_id) as message_count
+FROM messages
+where extract(month from sent_date)=8
+and extract(year from sent_date)=2022
+group by sender_id
+order by count(message_id) desc
+limit 2;
 
+/*Ex6: find the IDs of the invalid tweets.
+The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15 */
 
+select tweet_id
+from tweets
+where length(content) >15;
+
+/*Ex7:find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
+A user was active on someday if they made at least one activity on that day */
+
+select activity_date as day,
+count(distinct user_id) as active_users
+from activity
+where activity_date between '2019-06-28' and '2019-07-28'
+group by activity_date;
+
+/*ex8: find the number of employees hired between the months of January and July in the year 2022 inclusive */
+
+select count(id) as number_of_employees
+from employees
+where extract(month from joining_date) between 1 and 7
+and extract(year from joining_date)=2022;
+
+/*ex9: position of the lower case letter 'a' in the first name of the worker 'Amitah' */
+
+select position('a' in first_name)
+from worker
+where first_name ='Amitah';
+
+/*ex10: Find the vintage years of all wines from the country of Macedonia. 
+The year can be found in the 'title' column.
+Output the wine (i.e., the 'title') along with the year*/
+
+select winery, 
+substring(title, length(winery)+2,4) --extract the year from 'title'
+                                     --year goes after winery --> length(winery)+2(space+first_number), lay 4 chu so
+from winemag_p2
+where country ='Macedonia';
 
 
 
