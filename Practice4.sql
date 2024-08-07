42405c60748573916a9c41d17682bc90ecf05e56
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-06 23:25:01 -0700

    Update Practice4.sql

--- Practice4.sql
+++ Practice4.sql
@@ -30,4 +30,18 @@ select name
 from customer
 where referee_id <> 2 or referee_id is null;
 
+/*ex5: showing the number of survivors and non-survivors by passenger class.
+Classes are categorized based on the pclass value as:
+pclass = 1: first_class
+pclass = 2: second_classs
+pclass = 3: third_class
+Output the number of survivors and non-survivors by each class */
+
+select survived, 
+sum(case when pclass=1 then 1 else 0 end) as first_class, 
+sum(case when pclass=2 then 1 else 0 end) as second_class,
+sum(case when pclass=3 then 1 else 0 end) as third_class
+from titanic
+group by survived
+
 
