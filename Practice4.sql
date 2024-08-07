c0cf25c4d29d25312c9686290489f5838fe982f6
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-06 23:16:32 -0700

    Update Practice4.sql

--- Practice4.sql
+++ Practice4.sql
@@ -1,1 +1,33 @@
-/*ex1: 
+/*ex1: calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership.
+Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views. */
+
+SELECT 
+sum(case when device_type='laptop' then 1 else 0 end) as laptop_views,
+sum(case when device_type='tablet' or device_type='phone' then 1 else 0 end) as mobile_views
+FROM viewership;
+
+/*ex2: Report for every three line segments whether they can form a triangle. */
+
+select x,y,z, 
+case when x+y>z and x+z>y and y+z>x then 'Yes' else 'No'
+end as triangle 
+from triangle;
+
+/*ex3: some calls cannot be neatly categorised.
+These uncategorised calls are labeled as “n/a”, or are left empty when the support agent does not enter anything into the call category field.
+Write a query to calculate the percentage of calls that cannot be categorised. Round your answer to 1 decimal place */
+
+SELECT 
+round(sum(case
+when call_category='n/a' then 1 
+when call_category is null then 1
+else 0 end)*100.0/count(case_id),1) as uncategorised_call_pct
+from callers;
+
+/ex4: Find the names of the customer that are not referred by the customer with id = 2*/
+
+select name
+from customer
+where referee_id <> 2 or referee_id is null;
+
+
