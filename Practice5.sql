c2653a5c9a56f9a7dd92b7d2d9733ce9549ec16c
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-08 10:50:04 -0700

    Create Practice5.sql

---
+++ Practice5.sql
@@ -0,0 +1,4 @@
+/*CITY and COUNTRY tables, 
+query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population)
+rounded down to the nearest integer */
+
