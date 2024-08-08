bfab84c4fae8f20565f261efd3bbdc351529ee04
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-08 11:02:12 -0700

    Update Practice5.sql

--- Practice5.sql
+++ Practice5.sql
@@ -2,3 +2,8 @@
 query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population)
 rounded down to the nearest integer */
 
+select a.continent, floor(avg(b.population)) as avg_population --question require ROUND DOWN--
+from country a
+join city b on a.code=b.countrycode
+group by a.continent
+order by round(avg(b.population),0) asc
