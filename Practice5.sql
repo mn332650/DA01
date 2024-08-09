d7e17a5edd0900a556458c58093a2991dfb435ca
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-08 19:30:22 -0700

    Update Practice5.sql

--- Practice5.sql
+++ Practice5.sql
@@ -6,4 +6,18 @@ select a.continent, floor(avg(b.population)) as avg_population --question requir
 from country a
 join city b on a.code=b.countrycode
 group by a.continent
-order by round(avg(b.population),0) asc
+order by round(avg(b.population),0) asc;
+
+/*ex2: ew TikTok users sign up with their emails. 
+They confirmed their signup by replying to the text confirmation to activate their accounts.
+Users may receive multiple text messages for account confirmation until they have confirmed their new account.
+A senior analyst is interested to know the activation rate of specified users in the emails table. 
+Write a query to find the activation rate. Round the percentage to 2 decimal places */
+
+select
+round(sum(case when t.signup_action='Confirmed' then 1 else 0 end)::decimal/count(distinct e.email_id),2)
+as activation_rate
+from emails e  
+left join texts t on e.email_id=t.email_id;
+
+
