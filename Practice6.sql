dad7295c43345bb15924f88a00811e99c483e703
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-10 22:25:27 -0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -12,3 +12,42 @@ group by company_id, title, description --which companies have duplicate jobs, j
 select count(distinct company_id) as duplicate_companies
 from duplicate_jobs
 where job_counts>1 --don't have group by after bc use count right after SELECT 
+
+/*ex2: */
+
+
+
+/*ex3: query to find how many UHG policy holders made three, or more calls, assuming each call is identified by the case_id column */
+
+with call_counts as 
+(
+select policy_holder_id, 
+count(case_id) as call_counts
+from callers 
+group by policy_holder_id 
+having count(case_id) >=3) --count how many policy_holder having 3 or more calls 
+select count(policy_holder_id) as policy_holder_count 
+from call_counts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
