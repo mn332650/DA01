16cddfb4f3fc62ff0e68f475e3a7769fe9d50fc4
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-10 22:31:55 -0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -27,16 +27,16 @@ from callers
 group by policy_holder_id 
 having count(case_id) >=3) --count how many policy_holder having 3 or more calls 
 select count(policy_holder_id) as policy_holder_count 
-from call_counts
+from call_counts;
 
+/*ex4: Write a query to return the IDs of the Facebook pages that have zero likes. 
+The output should be sorted in ascending order based on the page IDs */
 
-
-
-
-
-
-
-
+SELECT a.page_id
+FROM pages a  --contains page_id, page_name
+left join page_likes b on a.page_id=b.page_id --contains user_id, page_id, liked_date
+where b.page_id is null --left join to retrieve all page_id from table a and page_id from table b is null=it appears in table a but not in table b bc table 2 contains liked_date info
+order by a.page_id asc;
 
 
 
