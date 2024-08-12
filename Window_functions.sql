6f60b242cb881cdf03d8cf8efcff2cea73042909
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-12 09:17:39 -0700

    Update Window_functions.sql

--- Window_functions.sql
+++ Window_functions.sql
@@ -1,1 +1,9 @@
+--WINDOW FUNCTIONS: 
+--PARTITION: phan cum # GROUP BY: gom nhom 
+--Mot khoi = 1 window, moi dong gia tri trong 1 cua so same
 
+/* SUM(FIELD) OVER (PARTITION BY FIELD) AS NAME: tong hop field over chia thanh khoi chia theo FIELD va dat ten theo name*/
+
+--Tong hop: SUM, AVG, MIN, MAX, COUNT
+--Xep hang: RANK
+--Phan tich: LEAD, LAG
