e43cbe6a26e0e5db864e55919912e87b19b165c6
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-12 18:53:20 -0700

    Update Practice7.sql

--- Practice7.sql
+++ Practice7.sql
@@ -1,1 +1,21 @@
+/*ex1: Assume you're given a table containing information about Wayfair user transactions for different products. 
+Write a query to calculate the year-on-year growth rate for the total spend of each product, grouping the results by product ID.
+The output should include the year in ascending order, product ID, current year's spend, previous year's spend and year-on-year growth percentage,
+rounded to 2 decimal places*/
+
+with cte_prev_spend AS
+(
+select extract(year from transaction_date) as year,
+product_id, 
+spend as curr_year_spend,
+lag(spend) over(partition by product_id order by extract(year from transaction_date)) as prev_year_spend
+from user_transactions
+) 
+select year, 
+product_id, 
+curr_year_spend, 
+prev_year_spend, 
+round((curr_year_spend-prev_year_spend)/prev_year_spend*100,2) as yoy_rate
+from cte_prev_spend;
+
 
