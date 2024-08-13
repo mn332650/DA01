1901baf325492e445b757cef830febdd727b43b3
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-13 16:19:32 -0700

    Update Practice8.sql

--- Practice8.sql
+++ Practice8.sql
@@ -17,3 +17,30 @@ join cte_first_order_date a
 on a.customer_id=b.customer_id
 and a.date1=b.order_date;
 
+/*ex2: Write a solution to report the fraction of players that logged in again on the day after the day they first logged in,
+rounded to 2 decimal places. In other words, you need to count the number of players that logged in
+for at least two consecutive days starting from their first login date, then divide that number by the total number of players*/
+
+
+
+
+
+
+
+
+/*ex3: Write a solution to swap the seat id of every two consecutive students.
+If the number of students is odd, the id of the last student is not swapped.
+Return the result table ordered by id in ascending order. */
+
+with seat_rank as 
+(
+select id, student, 
+row_number() over(order by id) as seat_rank
+from seat --rank the seat number order by student id
+)  
+select id, 
+coalesce( --return the first non-null value in the list
+    case when seat_rank%2=0 then lag(student) over()         --even number, return the previous stud
+    else lead(student) over()                                --else=odd number, return the next student
+    end, student) as student
+from seat_rank;
