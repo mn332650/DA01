c193e0328c45d44a6d3d0b58353791780288fc24
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-20 15:28:30 -0700

    Create Segmentation_analysis.sql

---
+++ Segmentation_analysis.sql
@@ -0,0 +1,22 @@
+/* Segmentation Analysis (phan loai kh): chien luoc phan tich, chia nho kh thanh nhung nhom doi tuong co chung cac dac diem va tieu chi
+Include: Demographics (nhan khau), geographic (dia li), psychologic (tam li), behavioral (hanh vi)
+
+RFM (Recency - Frequency - Monetary):
+    + Lan gan day kh giao dich? 
+    + KH co thuong xuyen giao dich khong?
+    + Moi lan KH giao dich bao nhieu tien?
+
+B1: Tinh gia tri R-F-M
+1. Recency: khoang thoi gian tinh tu lan cuoi giao dich toi hien tai.
+R= ngay phan tich - ngay giao dich gan nhat
+2. Frequency: tan suat giao dich
+F= tong so lan mua hang/(ngay giao dich gan nhat - ngay giao dich dau tien)
+3. Monetary: tong so tien KH da chi tra
+M= cong gop so tien ma KH da thanh toan
+
+B2: chia cac gia tri thanh cac khoang tren thang diem 1-5(refer to Q1, Q2, Q3 and IQR)
+
+
+
+
+
