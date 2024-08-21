160f92d5bf5ebb25da427f7cf3da59f9adeacdbe
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-20 22:15:01 -0700

    Update Segmentation_analysis.sql

--- Segmentation_analysis.sql
+++ Segmentation_analysis.sql
@@ -15,6 +15,16 @@ F= tong so lan mua hang/(ngay giao dich gan nhat - ngay giao dich dau tien)
 M= cong gop so tien ma KH da thanh toan
 
 B2: chia cac gia tri thanh cac khoang tren thang diem 1-5(refer to Q1, Q2, Q3 and IQR)
+B3: Phan nhom theo 125 to hop E-F-M (5 groups R, 5 Fs, 5 Ms = 5x5x5)
+B4: Truc quan phan tich RFM: answer which customer group is the biggest
+B5: Dua vao phan tich RFM va dua ra marketing campaigns
+
+-Group 1: Champions: frequent purchases and high retention rate > de xuat chuong trinh than thiet voi gia tri khac biet va tinh ca nhan hoa cao
+-Group 2: Loyal Customers and Potential Loyalist > chuong trinh uu dai gan lien voi cac nguong chi tieu hoac chuong trinh gioi thieu ban be
+-Group 3: Recent Customers & Promising Customers > How to make them become loyal > Business create quy trinh cham soc bai ban va duoc ca nhan hoa cao de tao dung mqh
+-Group 4: Customers Needing Attention & At Risk > Why customers used to buy but not buy anymore ? Research to find reasons
+-Group 5: Customers About To Sleep, Hibernating & Lost > retargeting campaigns, short promotion like discount, voucher, etc
+
 
 
 
