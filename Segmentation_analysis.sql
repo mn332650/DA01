574c9fd8dd7b4076e3550bc21aedb7ac20ef4bcb
mn332650 <168997760+mn332650@users.noreply.github.com>
2024-08-20 22:15:35 -0700

    Update Segmentation_analysis.sql

--- Segmentation_analysis.sql
+++ Segmentation_analysis.sql
@@ -23,8 +23,52 @@ B5: Dua vao phan tich RFM va dua ra marketing campaigns
 -Group 2: Loyal Customers and Potential Loyalist > chuong trinh uu dai gan lien voi cac nguong chi tieu hoac chuong trinh gioi thieu ban be
 -Group 3: Recent Customers & Promising Customers > How to make them become loyal > Business create quy trinh cham soc bai ban va duoc ca nhan hoa cao de tao dung mqh
 -Group 4: Customers Needing Attention & At Risk > Why customers used to buy but not buy anymore ? Research to find reasons
--Group 5: Customers About To Sleep, Hibernating & Lost > retargeting campaigns, short promotion like discount, voucher, etc
+-Group 5: Customers About To Sleep, Hibernating & Lost > retargeting campaigns, short promotion like discount, voucher, etc */
 
+EXAMPLE: 
+--B1: find R-F-M
+--R= ngay phan tich - ngay giao dich gan nhat (max(order_date))
+--F=tong so lan mua hang/(ngay giao dich gan nhat - ngay giao dich dau tien) --count(order_id)
+--M=cong gop so tien ma KH da thanh toan
+
+with customer_RFM as 
+(
+select a.customer_id, 
+current_date-max(order_date) as R,
+count(distinct order_id) as F, 
+sum(sales) as M
+from customer a
+join sales b on a.customer_id=b.customer_id
+group by a.customer_id),
+
+--B2: chia cac gia tri thanh cac khoang tren thang diem 1-5
+RFM_score as
+	(select customer_id, 
+ntile(5) over(order by R desc) as R_score, --the bigger R is=big gap from last transaction
+ntile(5) over(order by F asc)	as F_score,
+ntile(5) over(order by M asc) as M_score
+from customer_RFM
+)
+
+--B3: Phan nhom theo 125 to hop R-F-M
+, rfm_final as 
+	(
+	select 
+customer_id,
+cast(r_score as varchar)||cast(F_score as varchar)||cast(m_score as varchar) as RFM_score --can't substring int > cast to varchar
+from rfm_score)
+
+select segment, count(*) from --count #customers each segment
+(select a.customer_id, b.segment
+from rfm_final a join segment_score b on a.rfm_score=b.scores) as a --phan nhom customer to each segment
+group by segment
+order by count(*) 
+
+--Insights: At risk group is the most=customers used to buy but don't come back
+--Focus on champions group!
+
+--B4: truc quan phan tich RFM - bieu do Hitmap
+--Download excel > insert > chart >treemap (top row, middle chart)
 
 
 
